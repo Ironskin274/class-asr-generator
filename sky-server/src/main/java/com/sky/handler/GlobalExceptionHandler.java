96ccafd2553a94a70682f833ffc4787a50c8aa38
LiYing <ying.li03@live.cn>
2025-02-17 13:13:07 +0800

    新增员工代码

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -12,7 +15,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-
+    /**
+     * 捕获数据库重复业务异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        String message = ex.getMessage();
+        if(message.contains("Duplicate entry")){
+            String[] split = message.split(" ");
+            String username = split[2];
+            String msg = username + MessageConstant.ALREADY_EXIST;
+            System.out.println(msg);
+            log.error(msg);
+            return Result.error(msg);
+        }
+        return Result.error(MessageConstant.UNKNOWN_ERROR);
+    }
     /**
      * 捕获业务异常
      * @param ex
@@ -23,5 +43,7 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
+
+
 
 }
